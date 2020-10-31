674fdf0fa21ecb3c4fe54ebd1a39fc080b324f1e
BrijeshVP <57172793+brijeshvp@users.noreply.github.com>
2020-10-31 09:41:34 -0400

    Create print all odd numbers in list.py

---
+++ print all odd numbers in list.py
@@ -0,0 +1,14 @@
+ 
+list1 = [10, 21, 4, 45, 66, 93] 
+i = 0
+
+# using while loop		 
+while(i < len(list1)): 
+	
+	# checking condition 
+	if list1[i] % 2 != 0: 
+		print(list1[i], end = " ") 
+	
+	# increment i 
+	i += 1
+	
